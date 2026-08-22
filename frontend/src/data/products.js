40b477ba65aed3bdd8a87897424326ffd0c236b8
delaFuenteJM <delajuanma@gmail.com>
2026-08-21 21:59:13 -0300

    Update product prices and details for '1 Kg' and '1KG + 1/2'

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -1,6 +1,6 @@
 export const PRODUCTS = [
-  { id: '1', name: '1 Kg', price: 18500, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 1000},
-  { id: '2', name: '1/2 kg', price: 11000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 500 },
+  { id: '1', name: '1 Kg', price: 20000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 1000},
+  { id: '2', name: '1/2 kg', price: 12000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 500 },
   { id: '3', name: '1/4 kg', price: 7000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 250 },
   { id: '4', name: '1 bocha', price: 2000, category: 'Ice Cream', icon: 'IceCream', weightInGrams: 50 },
   { id: '5', name: '2 Bocha', price: 3500, category: 'Ice Cream', icon: 'IceCream', weightInGrams: 100 },
@@ -13,6 +13,6 @@ export const PRODUCTS = [
   { id: '12', name: 'Delivery', price: 3000, category: 'Pastry', icon: 'Bike', weightInGrams: 0, excludeFromClosing: true },
   { id: '13', name: 'cucurucho', price: 300, category: 'Ice Cream', icon: 'IceCream', weightInGrams: 0 },
   { id: '14', name: 'vasito', price: 150, category: 'Ice Cream', icon: 'IceCream', weightInGrams: 0 },
-  { id: '15', name: 'Promo Mund', price: 25000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 1500 },
+  { id: '15', name: '1KG + 1/2', price: 32000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 1500 },
   { id: '16', name: '3x2 1/4 kg', price: 14000, category: 'Ice Cream', icon: 'IceCreamBowl', weightInGrams: 750 }
 ];
